0afe8ca74e836ecd5563e8b84193fa55a4f7a61d
paul <mannion.paul@gmail.com>
2014-03-26 19:42:54 +0000

    php file added for push to heroku

--- test/spec/ConverterSpec.js
+++ test/spec/ConverterSpec.js
@@ -1,1 +1,17 @@
 
+
+describe ("My Converter", function() {
+
+    it("should be able to deal with strings", function() {
+        expect(function()
+        {Converter.convertFromImperialToMetric("hello")}).toThrow(new Error("not a number"));
+    })
+
+    it("should be a positive number", function(){
+        expect(function()
+        {Converter.checkIsPositiveNumber(-2)}).toThrow(new Error("not a positive number"));
+    })
+
+
+
+})
